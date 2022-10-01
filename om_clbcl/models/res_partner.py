e2b318f6da6a06599bb5a3549b843d68380ac8e3
huongtn <huongkstn@gmail.com>
2022-10-01 17:34:46 +0700

    đfdf
    fdfd

--- om_clbcl/models/res_partner.py
+++ om_clbcl/models/res_partner.py
@@ -6,4 +6,6 @@ from odoo import api, fields, models
 class ResPartner(models.Model):
     _inherit = "res.partner"
     default_partner_shipping_id = fields.Integer(string='Default Partner Shipping ID')
-
+    province_id = fields.Many2one("clbcl.province", string='province')
+    district_id = fields.Many2one("clbcl.district", string='district')
+    ward_id = fields.Many2one("clbcl.ward", string='ward')
