ed29684ce18b0f00c0409bf73da1990edf839db6
huongtn <huongkstn@gmail.com>
2022-10-10 15:26:30 +0700

    Delete models directory
    Delete

--- models/club_user.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import api, fields, models
-
-
-class ClubUser(models.Model):
-    _inherit = "res.users"
-    club_id = fields.Many2one("clbcl.club", string='Club')
-
