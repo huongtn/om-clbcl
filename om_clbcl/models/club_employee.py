c2969817af36f6ba29d998ed114f5c7e0b4b8fb0
huongtn <huongkstn@gmail.com>
2022-10-10 15:25:41 +0700

    Delete om_clbcl directory
    Delete

--- om_clbcl/models/club_employee.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import api, fields, models
-
-
-class HrEmployee(models.Model):
-    _inherit = "hr.employee"
-    club_id = fields.Many2one("clbcl.club", string='Club')
-
--- om_clbcl/models/product_category.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import api, fields, models
-
-
-class ProductCategory(models.Model):
-    _inherit = "product.category"
-    image_128 = fields.Image(string="Image")
-    image_128_active = fields.Image(string="Image Active")
