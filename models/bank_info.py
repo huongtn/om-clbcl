61b1a0f7a5137c5b87b8a34e189172da5bddb28f
kelvin <huongkstn@gmail.com>
2022-10-21 18:59:06 +0700

    Add image to bank info

--- models/bank_info.py
+++ models/bank_info.py
@@ -7,6 +7,7 @@ class CLBCLBankInfo(models.Model):
     _name = "clbcl.bank.info"
     _description = "CLBCL Bank Info"
     bank_name = fields.Text(string="Tên ngân hàng")
+    image_url = fields.Text(string="Ảnh")
     bank_account = fields.Text(string="Tên chủ tài khoản")
     bank_account_number = fields.Text(string="Số tài khoản")
     payment_content = fields.Text(string="Nội dung chuyển khoản")
