56482f21acd64e0c3b5ad01cf2cb5df2a7d59d3b
kelvin <huongkstn@gmail.com>
2022-12-26 15:49:07 +0700

    Update get product details

--- controllers/main.py
+++ controllers/main.py
@@ -547,6 +547,7 @@ class CLBCLController(http.Controller):
     @http.route('/get_product_details', type='json', auth='public', methods=['POST'], website=True, sitemap=False)
     def get_product_details(self, **rec):
         product = request.env['product.product'].search_read([('id', '=', rec['product_id'])])
+        product_template = request.env['product.template'].search_read([('id', '=', product[0]['product_tmpl_id'])])
         attributes = request.env['product.template.attribute.value'].search_read([('id', 'in', product[0]['product_template_variant_value_ids'])])
         all_attributes = []
         for attribute in attributes:
@@ -579,8 +580,5 @@ class CLBCLController(http.Controller):
             "summary1":product[0]['summary1'],
             "summary2":product[0]['summary2'],
             "summary3":product[0]['summary3'],
-            "area":product[0]['area'],
-            "style":product[0]['style'],
-            "grape_type":product[0]['grape_type'],
             "public_categ_ids":product[0]['public_categ_ids'],
-        }, 'attributes': all_attributes}
+        }, 'attributes': all_attributes, 'product_template': product_template}
